803fa2be611beb72acc49d9f2685ed4e44d3d0a2
Haeryz <haeriz42069@gmail.com>
2025-01-25 17:14:38 +0700

    Remove unused Navbar import from App component

--- portofolio/src/App.tsx
+++ portofolio/src/App.tsx
@@ -2,7 +2,6 @@ import { Box } from "@chakra-ui/react"
 import { StarryBackground } from "./components/StarryBackground"
 import { ThemeProvider } from "./contexts/ThemeContext"
 import Nama from "./components/Nama"
-import Navbar from "./components/Navbar"
 import BottomNav from "./components/BottomNav"
 import ProfileWidget from "./components/ProfileWidget"
 
