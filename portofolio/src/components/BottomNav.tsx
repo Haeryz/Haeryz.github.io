53e0bd8734f16e476f207053e6d3e9f32a0fd5dd
Haeryz <haeriz42069@gmail.com>
2025-01-26 07:37:34 +0700

    Remove unnecessary React imports from BottomNav and ProfileWidget components

--- portofolio/src/components/BottomNav.tsx
+++ portofolio/src/components/BottomNav.tsx
@@ -1,5 +1,4 @@
 import { Flex, HStack, IconButton } from '@chakra-ui/react'
-import React from 'react'
 import { MdOutlineWavingHand } from 'react-icons/md'
 import { useTheme } from '../contexts/ThemeContext'
 import { Tooltip } from './ui/tooltip'
