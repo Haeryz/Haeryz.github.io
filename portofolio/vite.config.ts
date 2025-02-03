22d7b515ac35c29de11c8731c7bb55c66ef61fb7
Haeryz <haeriz42069@gmail.com>
2025-02-03 11:52:34 +0700

    Update deployment workflow to copy index.html to 404.html and adjust base path in Vite config

--- portofolio/vite.config.ts
+++ portofolio/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "/Haeryz.github.io/",
+  base: '/',  // Change this from "/Haeryz.github.io/" to "/"
   build: {
     chunkSizeWarningLimit: 1000, // Increase size limit to 1000kb
     rollupOptions: {
