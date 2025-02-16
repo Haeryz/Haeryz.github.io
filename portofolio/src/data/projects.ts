a63ceb83ba95098cb9771e3f139e5ffcea6ab438
Haeryz <haeriz42069@gmail.com>
2025-02-16 16:54:50 +0700

    Added Project

--- portofolio/src/data/projects.ts
+++ portofolio/src/data/projects.ts
@@ -1,63 +1,60 @@
-
 import { Project } from '../types/project';
-import Ligmapp from "../assets/projects/Ligmapp.png";
-import Kozzila from "../assets/projects/Kozzila.png";
 
 export const projects: Project[] = [
     {
         id: "1",
         title: "Ligmapp",
         description: "Travel management app integrated with Google Maps API, Firebase auth, Firebase storage, and Firestore",
-        image: Ligmapp,
-        tags: ["Mobile", "Flutter", "Dart", "Firebase", "API", "Google Maps"],
+        image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tags: ["Mobile", "Flutter", "Dart", "Firebase", "GetX", "Google Maps"],
         link: "https://github.com/Haeryz/Skibidi-skibidi-sigma/releases/tag/release"
     },
     {
         id: "2",
-        title: "Kozzila",
-        description: "Travel management app ",
-        image: Kozzila,
-        tags: ["Mobile", "Flutter"],
-        link: "https://github.com/Haeryz/Skibidi-skibidi-sigma/releases/tag/release"
+        title: "Construction materials E-Commerce web app",
+        description: "E-Commerce web app that specifically sell construction materials to consumer",
+        image: "https://images.unsplash.com/photo-1591588582259-e675bd2e6088?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tags: ["MERN", "Websocket", "Zustand", "ChakraUI", "Zod", "Vite"],
+        link: "https://github.com/Haeryz/E-Commerce-depo-79"
     },
     {
         id: "3",
-        title: "Unity VR Fruit ninja",
-        description: "Fruit ninja VR clone that i made myself with Unity and XR Interaction Toolkit",
-        image: Kozzila,
-        tags: ["Unity", "VR", "XR"],
-        link: "https://github.com/Haeryz/Fruit-ninja"
+        title: "Home Alarm IoT Dashboard",
+        description: "Dashboard for my personal home alarm IoT using esp32",
+        image: "https://images.unsplash.com/photo-1558346490-a72e53ae2d4f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tags: ["MERN", "Websocket", "Zustand", "ChakraUI", "Arduino", "Vite", "IoT"],
+        link: "https://github.com/Haeryz/Hawktuah"
     },
     {
         id: "4",
-        title: "Library Management System",
-        description: "Fruit ninja VR clone that i made myself with Unity and XR Interaction Toolkit",
-        image: Kozzila,
-        tags: ["Django", "Python", "SQLite"],
-        link: "https://github.com/Haeryz/Library-Management"
+        title: "Boarding house management app",
+        description: "App that manage your boarding house expenses",
+        image: "https://images.unsplash.com/photo-1612637968894-660373e23b03?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YXBhcnRtZW50JTIwYnVpbGRpbmd8ZW58MHx8MHx8fDA%3D",
+        tags: ["Flutter", "Firebase", "GetX"],
+        link: "https://github.com/Haeryz/kozzzila"
     },
     {
         id: "5",
-        title: "BasorAI",
-        description: "Fruit ninja VR clone that i made myself with Unity and XR Interaction Toolkit",
-        image: Kozzila,
-        tags: ["Django", "Python", "SQLite"],
-        link: "https://github.com/Haeryz/adging"
+        title: "Perfume E-Commerce web app",
+        description: "Webapp that sell perfume",
+        image: "https://images.unsplash.com/photo-1545840716-c82e9eec6930?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8UGVyZnVtZXxlbnwwfHwwfHx8MA%3D%3D",
+        tags: ["PHP", "Laravel", "Livewire", "Sanctum", "MySQL","ChakraUI", "React", "Zustand"],
+        link: "https://github.com/andifathir/Roven"
     },
     {
         id: "6",
-        title: "IoT Dashboard",
-        description: "Fruit ninja VR clone that i made myself with Unity and XR Interaction Toolkit",
-        image: Kozzila,
-        tags: ["NodeJS", "MongoDB", "ExpressJS", "ReactJS", "ESP32", "C", "Zustand"],
-        link: "https://github.com/Haeryz/adging"
+        title: "Library management web app",
+        description: "Web that manage books and borrowing",
+        image: "https://images.unsplash.com/photo-1521587760476-6c12a4b040da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8TGlicmFyeXxlbnwwfHwwfHx8MA%3D%3D",
+        tags: ["Python", "Django", "sqlite3", "Python barcode", "HTML", "CSS", "JS"],
+        link: "https://github.com/Haeryz/Library-Management"
     },
     {
         id: "7",
-        title: "Roven",
-        description: "Fruit ninja VR clone that i made myself with Unity and XR Interaction Toolkit",
-        image: Kozzila,
-        tags: ["Laravel", "MySQL", "ReactJS"],
-        link: "https://github.com/Haeryz/adging"
+        title: "VR Fruit ninja clone",
+        description: "Personal project to learn VR development using Unity and Meta XR all in one sdk",
+        image: "https://images.unsplash.com/photo-1592478457432-38ed2af3ee03?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        tags: ["Unity", "C#", "Meta XR", "VR"],
+        link: "https://github.com/Haeryz/Fruit-ninja"
     },
 ];
